9e3c15d831aa5a9f74a636d02076653fd49a6f1c
lyflyy <lyflyyvip@163.com>
2020-01-15 01:38:40 +0800

    init project

---
+++ src/main/java/com/uyibai/uboot/uboot/UbootApplication.java
@@ -0,0 +1,13 @@
+package com.uyibai.uboot.uboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uyibai/uboot/uboot/UbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uyibai.uboot.uboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
