e1079993416c541787ed3d3c3b28e14c584b1de2
lyflyy <lyflyyvip@163.com>
2020-02-15 13:29:43 +0800

    update: sql解析变量提取，防止并发问题

--- uboot-module-system/src/main/java/org/uboot/config/mybatis/permission/ParseSqlVariable.java
+++ uboot-module-system/src/main/java/org/uboot/config/mybatis/permission/ParseSqlVariable.java
@@ -18,7 +18,6 @@ import java.util.Vector;
  * @Email: lyflyyvip@163.com
  * @create: 2020-02-13 01:37
  * @Description:
- * todo - 这个对象，spring bean直接获取，并且每一次操作都会修改对象的属性，会有并发的问题
  **/
 public abstract class ParseSqlVariable {
 
@@ -34,53 +33,4 @@ public abstract class ParseSqlVariable {
 
     static final Logger logger = LoggerFactory.getLogger(ParseSql.class);
 
-    /**
-     * sql的主要查询的主体
-     */
-    protected PlainSelect selectBody = null;
-
-    /**
-     * sql处理处理之前被
-     * 符合条件的表的alias名称
-     * 可能为空
-     */
-    protected String oldAliasName = null;
-
-    /**
-     * sql处理处理之前被
-     * 符合条件的表的alias对象
-     * 可能为空
-     */
-    protected Alias oldAlias = null;
-
-    /**
-     * 给符合条件的表设置的alias
-     */
-    protected Alias newAlias = null;
-
-    /**
-     * sql解析 jsqlparser操作主体
-     */
-    protected Select select = null;
-
-    /**
-     * 主表，被left join 的表
-     * 都通过get set去调用
-     */
-    protected FromItem fromItem = null;
-
-    /**
-     * 被操作的主表
-     */
-    protected Table fromTable = null;
-
-    /**
-     * 该sql所操作的所有的表
-     */
-    protected Vector<Table> tableList = new Vector<>();
-
-    /**
-     * 该sql所关联的所有表，包括子查询表
-     */
-    protected List<Join> joins;
 }
---
+++ uboot-module-system/src/main/java/org/uboot/config/mybatis/permission/ParseSqlVo.java
@@ -0,0 +1,74 @@
+package org.uboot.config.mybatis.permission;
+
+import lombok.Data;
+import net.sf.jsqlparser.expression.Alias;
+import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.select.FromItem;
+import net.sf.jsqlparser.statement.select.Join;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
+
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * @author: LiYang
+ * @Email: lyflyyvip@163.com
+ * @create: 2020-02-14 15:39
+ * @Description:
+ **/
+@Data
+public class ParseSqlVo {
+
+    /**
+     * sql的主要查询的主体
+     */
+    protected PlainSelect selectBody = null;
+
+    /**
+     * sql处理处理之前被
+     * 符合条件的表的alias名称
+     * 可能为空
+     */
+    protected String oldAliasName = null;
+
+    /**
+     * sql处理处理之前被
+     * 符合条件的表的alias对象
+     * 可能为空
+     */
+    protected Alias oldAlias = null;
+
+    /**
+     * 给符合条件的表设置的alias
+     */
+    protected Alias newAlias = null;
+
+    /**
+     * sql解析 jsqlparser操作主体
+     */
+    protected Select select = null;
+
+    /**
+     * 主表，被left join 的表
+     * 都通过get set去调用
+     */
+    protected FromItem fromItem = null;
+
+    /**
+     * 被操作的主表
+     */
+    protected Table fromTable = null;
+
+    /**
+     * 该sql所操作的所有的表
+     */
+    protected Vector<Table> tableList = new Vector<>();
+
+    /**
+     * 该sql所关联的所有表，包括子查询表
+     */
+    protected List<Join> joins;
+
+
+}
