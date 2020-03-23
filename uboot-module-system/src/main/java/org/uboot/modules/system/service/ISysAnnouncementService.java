74f92cb98b4f72d2c555a793c4f2f1435b68656a
Huzi.Wang <huzi.wh@gmail.com>
2020-03-24 01:38:55 +0800

    update: 完善系统通知

--- uboot-module-system/src/main/java/org/uboot/modules/system/service/ISysAnnouncementService.java
+++ uboot-module-system/src/main/java/org/uboot/modules/system/service/ISysAnnouncementService.java
@@ -13,15 +13,15 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface ISysAnnouncementService extends IService<SysAnnouncement> {
 
-    /**
-     * 根据通知模版发送消息
-     * @param templateCode
-     * @param userIds
-     * @param currentUserName
-     */
-    void sendWithTemplate(String templateCode, String userIds, String currentUserName);
+	/**
+	 * 根据 模板发送消息
+	 *
+	 * @param templateCode 模板编号
+	 * @param parameters 可变长参数, 参数 至少 包含两个第一个 是 通知对象，第二个是 发送人
+	 */
+	void sendWithTemplate(String templateCode, String ...parameters);
 
-    boolean sendAnnouncement(SysAnnouncement sysAnnouncement, String currentUserName);
+	boolean sendAnnouncement(SysAnnouncement sysAnnouncement, String currentUserName);
 
 	public void saveAnnouncement(SysAnnouncement sysAnnouncement);
 
