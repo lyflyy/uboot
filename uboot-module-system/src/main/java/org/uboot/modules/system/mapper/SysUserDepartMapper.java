3d66d07153b682d170726f87687ca470d1e69a49
Huzi.Wang <huzi.wh@gmail.com>
2020-03-24 02:03:23 +0800

    update: 完善系统通知

--- uboot-module-system/src/main/java/org/uboot/modules/system/mapper/SysUserDepartMapper.java
+++ uboot-module-system/src/main/java/org/uboot/modules/system/mapper/SysUserDepartMapper.java
@@ -1,16 +1,16 @@
 package org.uboot.modules.system.mapper;
 
-import java.util.List;
-
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.uboot.modules.system.entity.SysUserDepart;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-import io.lettuce.core.dynamic.annotation.Param;
+import java.util.List;
 
 public interface SysUserDepartMapper extends BaseMapper<SysUserDepart>{
 
 	List<SysUserDepart> getUserDepartByUid(@Param("userId") String userId);
+
+    SysUserDepart getDepartAdminByDepId(@Param("depId") String depId);
 
     int deleteByDepCodeAndUser(@Param("deptCode")String deptCode,@Param("userId") String userId);
 }
